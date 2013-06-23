4491758f6b081701bfb92983b8b822f3d9b652f8
Gary V. Vaughan <gary@gnu.org>
2013-06-23 20:50:38 +0700

    lua: generate a .pc file
    Ensure there is a pkg-config file for lua in /usr/local/lib/pkgconfig.

--- lua52.rb
+++ lua52.rb
@@ -10,22 +10,26 @@ class Lua52 < Formula
     cause "Lua itself compiles with LLVM, but may fail when other software tries to link."
   end
 
-  option 'completion', 'Enables advanced readline support'
-  option 'sigaction', 'Signal handling improvements for interpreter'
+  option 'with-completion', 'Enables advanced readline support'
+  option 'without-sigaction', 'Revert to ANSI signal instead of improved POSIX sigaction'
 
-  # Be sure to build a dylib, or else runtime modules will pull in another static copy of liblua = crashy
-  # See: https://github.com/mxcl/homebrew/pull/5043
+  V = version.to_s.split('.')[0..1].join('.')
+
   def patches
-    p = [DATA]
-    # completion provided by advanced readline power patch from
-    # http://lua-users.org/wiki/LuaPowerPatches
-    if build.include? 'completion'
-      p << 'http://luajit.org/patches/lua-5.2.0-advanced_readline.patch'
-    end
+    p = []
+    # Be sure to build a dylib, or else runtime modules will pull in another static copy of liblua = crashy
+    # See: https://github.com/mxcl/homebrew/pull/5043
+    # Also, take care of versioned file suffixes to support parallel installation with other releases
+    p << 'https://gist.github.com/gvvaughan/5832054/raw/1f1d726cced4f23719de4aef24d965d257d6bbba/lua-5.2-homebrew.diff'
     # sigaction provided by posix signalling power patch from
     # http://lua-users.org/wiki/LuaPowerPatches
-    if build.include? 'sigaction'
+    unless build.without? 'sigaction'
       p << 'http://lua-users.org/files/wiki_insecure/power_patches/5.2/lua-5.2.2-sig_catch.patch'
+    end
+    # completion provided by advanced readline power patch from
+    # http://lua-users.org/wiki/LuaPowerPatches
+    if build.with? 'completion'
+      p << 'http://luajit.org/patches/lua-5.2.0-advanced_readline.patch'
     end
     p
   end
@@ -43,71 +47,27 @@ class Lua52 < Formula
 
     # this ensures that this symlinking for lua starts at lib/lua/5.2 and not
     # below that, thus making luarocks work
-    (HOMEBREW_PREFIX/"lib/lua"/version.to_s.split('.')[0..1].join('.')).mkpath
+    (HOMEBREW_PREFIX/"lib/lua/#{V}").mkpath
 
     system "make", "macosx", "INSTALL_TOP=#{prefix}", "INSTALL_MAN=#{man1}"
     system "make", "install", "INSTALL_TOP=#{prefix}", "INSTALL_MAN=#{man1}"
+    (lib+"pkgconfig/lua#{V}.pc").write pc_file
+    (lib+"pkgconfig/lua.pc").make_relative_symlink (lib+"pkgconfig/lua#{V}.pc")
+  end
+
+  def pc_file; <<-EOS.undent
+    prefix=#{opt_prefix}
+    exec_prefix=${prefix}
+    libdir=${exec_prefix}/lib
+    includedir=${prefix}/include/lua-#{V}
+
+    Name: Lua
+    Description: An Extensible Extension Language
+    Version: #{version}
+    Requires:
+    Libs: -L${libdir} -llua.#{V} -lm
+    Cflags: -I${includedir}
+    EOS
   end
 end
 
-__END__
-diff --git a/Makefile b/Makefile
-index bd9515f..5940ba9 100644
---- a/Makefile
-+++ b/Makefile
-@@ -41,7 +41,7 @@ PLATS= aix ansi bsd freebsd generic linux macosx mingw posix solaris
- # What to install.
- TO_BIN= lua luac
- TO_INC= lua.h luaconf.h lualib.h lauxlib.h lua.hpp
--TO_LIB= liblua.a
-+TO_LIB= liblua.5.2.2.dylib
- TO_MAN= lua.1 luac.1
- 
- # Lua version and release.
-@@ -63,6 +63,8 @@ install: dummy
- 	cd src && $(INSTALL_DATA) $(TO_INC) $(INSTALL_INC)
- 	cd src && $(INSTALL_DATA) $(TO_LIB) $(INSTALL_LIB)
- 	cd doc && $(INSTALL_DATA) $(TO_MAN) $(INSTALL_MAN)
-+	ln -s -f liblua.5.2.2.dylib $(INSTALL_LIB)/liblua.5.2.dylib
-+	ln -s -f liblua.5.2.dylib $(INSTALL_LIB)/liblua.dylib
- 
- uninstall:
- 	cd src && cd $(INSTALL_BIN) && $(RM) $(TO_BIN)
-diff --git a/src/Makefile b/src/Makefile
-index 8c9ee67..7f92407 100644
---- a/src/Makefile
-+++ b/src/Makefile
-@@ -28,7 +28,7 @@ MYOBJS=
- 
- PLATS= aix ansi bsd freebsd generic linux macosx mingw posix solaris
- 
--LUA_A=	liblua.a
-+LUA_A=	liblua.5.2.2.dylib
- CORE_O=	lapi.o lcode.o lctype.o ldebug.o ldo.o ldump.o lfunc.o lgc.o llex.o \
- 	lmem.o lobject.o lopcodes.o lparser.o lstate.o lstring.o ltable.o \
- 	ltm.o lundump.o lvm.o lzio.o
-@@ -56,11 +56,12 @@ o:	$(ALL_O)
- a:	$(ALL_A)
- 
- $(LUA_A): $(BASE_O)
--	$(AR) $@ $(BASE_O)
--	$(RANLIB) $@
-+	$(CC) -dynamiclib -install_name HOMEBREW_PREFIX/lib/liblua.5.2.dylib \
-+		-compatibility_version 5.2 -current_version 5.2.2 \
-+		-o liblua.5.2.2.dylib $^
- 
- $(LUA_T): $(LUA_O) $(LUA_A)
--	$(CC) -o $@ $(LDFLAGS) $(LUA_O) $(LUA_A) $(LIBS)
-+	$(CC) -fno-common $(MYLDFLAGS) -o $@ $(LUA_O) $(LUA_A) -L. -llua.5.2.2 $(LIBS)
- 
- $(LUAC_T): $(LUAC_O) $(LUA_A)
- 	$(CC) -o $@ $(LDFLAGS) $(LUAC_O) $(LUA_A) $(LIBS)
-@@ -106,7 +107,7 @@ linux:
- 	$(MAKE) $(ALL) SYSCFLAGS="-DLUA_USE_LINUX" SYSLIBS="-Wl,-E -ldl -lreadline -lncurses"
- 
- macosx:
--	$(MAKE) $(ALL) SYSCFLAGS="-DLUA_USE_MACOSX" SYSLIBS="-lreadline"
-+	$(MAKE) $(ALL) SYSCFLAGS="-DLUA_USE_MACOSX -fno-common" SYSLIBS="-lreadline"
- 
- mingw:
- 	$(MAKE) "LUA_A=lua52.dll" "LUA_T=lua.exe" \
